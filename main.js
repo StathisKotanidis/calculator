092d29a6b08220f29e06b18611f881cb8e2906a9
Stathis Kot <stathiskotanidhs@gmail.com>
2023-09-09 22:16:52 +0300

    changes in html and in my code logic

--- main.js
+++ main.js
@@ -1,182 +1,81 @@
-let display = document.querySelector('.screen')
-let zero = document.querySelector('#zero')
-let one = document.querySelector('#one')
-let two = document.querySelector('#two')
-let three = document.querySelector('#three')
-let four = document.querySelector('#four')
-let five = document.querySelector('#five')
-let six = document.querySelector('#six')
-let seven = document.querySelector('#seven')
-let eight = document.querySelector('#eight')
-let nine = document.querySelector('#nine')
-let addition = document.querySelector('#plus')
-let subtraction = document.querySelector('#minus')
-let multiplication = document.querySelector('#multiply')
-let division = document.querySelector('#divide')
-let equal = document.querySelector("#equal")
+let numbers = document.querySelectorAll('.numbers')
+let screen = document.querySelector('.screen')
+let operators = document.querySelectorAll('.operator')
+let deleteLastCharacter = document.querySelector('#delete')
 let clear = document.querySelector('#clear')
-let deleteLastDigit = document.querySelector('#delete')
+let equal = document.querySelector('#equal')
 
-let currentInput = []
+let currentInput = ''
+
+numbers.forEach((number) => number.addEventListener('click', event => {
+
+    currentInput += event.target.textContent
+    updateDisplay()
+}))
+
+operators.forEach((operator) => operator.addEventListener('click', event => {
+
+    currentInput += event.target.textContent
+    updateDisplay()
+}))
+
+clear.addEventListener('click', () => {
+
+    currentInput = ''
+    screen.textContent = 0
+})
+
+deleteLastCharacter.addEventListener('click', () => {
+
+    if (currentInput.length > 1) {
+        currentInput = currentInput.slice(0, -1)
+    } else {
+        currentInput = 0
+    }
+    updateDisplay()
+})
+
+equal.addEventListener('click', () => {
+
+    currentInput = splitingInput()
+    updateDisplay()
+
+})
+
+function updateDisplay() {
+
+    screen.textContent = currentInput //string
+}
 
 function operate(a, operator, b) {
 
-
-    if (operator == '+') {
+    if (operator == '＋') {
         return a + b
-    } else if (operator == '-') {
-        return a - b
-    } else if (operator == "*") {
-        return a * b
-    } else if (operator == '/') {
-        return a / b
+    } else if (operator == '－') {
+        return (a - b)
+    } else if (operator == "×") {
+        return (a * b)
+    } else if (operator == '÷') {
+        return (a / b)
     } else {
         console.log('Error')
     }
 
 }
 
-function updateDisplay() {
+function splitingInput() {
 
-    let displayValue = currentInput.join('')
-    display.textContent = displayValue
-}
+    let arrayForOperate = currentInput.split(/(＋|－|\×|÷)/);
+    let result = operate(parseFloat(arrayForOperate[0]), arrayForOperate[1], parseFloat(arrayForOperate[2]))
 
-zero.addEventListener('click', () => {
-    currentInput.push(0)
-    updateDisplay()
-});
-
-one.addEventListener('click', () => {
-    currentInput.push(1)
-    updateDisplay()
-});
-
-two.addEventListener('click', () => {
-    currentInput.push(2)
-    updateDisplay()
-});
-
-three.addEventListener('click', () => {
-    currentInput.push(3)
-    updateDisplay()
-})
-
-four.addEventListener('click', () => {
-    currentInput.push(4)
-    updateDisplay()
-})
-
-five.addEventListener('click', () => {
-    currentInput.push(5)
-    updateDisplay()
-});
-
-six.addEventListener('click', () => {
-    currentInput.push(6)
-    updateDisplay()
-});
-
-seven.addEventListener('click', () => {
-    currentInput.push(7)
-    updateDisplay()
-})
-
-eight.addEventListener('click', () => {
-    currentInput.push(8)
-    updateDisplay()
-})
-
-nine.addEventListener('click', () => {
-    currentInput.push(9)
-    updateDisplay()
-})
-
-clear.addEventListener('click', () => {
-
-    display.textContent = 0
-    currentInput = []
-
-})
-
-deleteLastDigit.addEventListener('click', () => {
-
-    currentInput = stringToArrayv2()
-    if (currentInput.length === 1) {
-        currentInput.pop()
-        display.textContent = 0
+    if (result === Math.floor(result)) {
+        return result.toString(); // Convert to string without decimal places
     } else {
-        currentInput.pop()
-        updateDisplay()
+        return result.toFixed(2); // Limit to two decimal places for floating-point numbers
     }
-
-})
-
-addition.addEventListener('click', () => {
-    if (currentInput.length > 0 && display.textContent !== '0') {
-        currentInput.push('+');
-        updateDisplay();
-    }
-});
-
-subtraction.addEventListener('click', () => {
-    if (currentInput.length > 0 && display.textContent !== '0') {
-        currentInput.push('-');
-        updateDisplay();
-    }
-});
-
-multiplication.addEventListener('click', () => {
-    if (currentInput.length > 0 && display.textContent !== '0') {
-        currentInput.push('*');
-        updateDisplay();
-    }
-});
-
-division.addEventListener('click', () => {
-    if (currentInput.length > 0 && display.textContent !== '0') {
-        currentInput.push('/');
-        updateDisplay();
-    }
-});
-
-equal.addEventListener('click', () => {
-
-    if (currentInput.length > 0 && display.textContent !== '0') {
-        stringToArray();
-        emptyingArray();
-        currentInput.push(display.textContent);
-    }
-
-});
-
-function emptyingArray() {
-
-    currentInput = []
 }
 
-function stringToArray() {
 
-    let outputString = currentInput.join('')
-    currentInput = outputString.split(/(\+|-|\*|\/)/);
-    display.textContent = operate(parseFloat(currentInput[0]), currentInput[1], parseFloat(currentInput[2]))
-}
 
-function stringToArrayv2() {
-
-    return display.textContent.split('');
-
-    /*  
-    i made this function to fix 2 buggs i had. The first one was that was when i tried to use the
-    delete button on an output it returned 0, eg i have 25*2=50. So my output is 50 and if i used
-    the delete button i should  had got 5 in my screen instead i was getting 0. This function
-    converts a string to array , eg "50" --> ['5','0'] and then i assigned this value at my 
-    currentInput value in my deleteLastDigit event listener.
-
-    The second bug it fixed it was that whenever i had 0 in my screen and  i pressed delete
-    button the 0 was removed and i had an empty screen which i didnt want.
-    Tha makes the screen look immutable when there is already a 0 on it.
-    */
-}
 
 
